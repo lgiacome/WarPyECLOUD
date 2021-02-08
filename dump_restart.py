0fd0acbc43153546295e1a2ab38daf363168d15e
lgiacome <lorenzo.giacomel@cern.ch>
2021-02-08 17:01:59 +0100

    fixing restart without antenna

--- dump_restart.py
+++ dump_restart.py
@@ -14,17 +14,20 @@ def dump(sim, filename):
     sim.chamber = None
     warpdump(filename)
 
-def reinit(sim, laser_func, custom_plot, custom_time_prof = None):
-    sim.laser_func = laser_func
-    sim.custom_plot = custom_plot
+def reinit(sim, laser_func = None, custom_plot = None, custom_time_prof = None):
+    if laser_func is not None:
+        sim.laser_func = laser_func
+        sim.solver.solver.laser_func = sim.laser_func
+        sim.solver.em3dfft_args['laser_func'] = sim.laser_func
+    if custom_plot is not None:
+        sim.custom_plot = custom_plot
+
     sim.custom_time_prof = custom_time_prof
     if custom_time_prof is None:
         sim.time_prof = sim.gaussian_time_prof
     else:
         sim.time_prof = sim.self_wrapped_custom_time_prof
 
-    sim.solver.solver.laser_func = sim.laser_func
-    sim.solver.em3dfft_args['laser_func'] = sim.laser_func
     sim.print_solvers_info() 
     sim.text_trap = {True: StringIO(), False: sys.stdout}[sim.enable_trap]
     sim.original = sys.stdout
