5dd2a926a4652fedb054de738d5e19ca784b984e
lgiacome <lorenzo.giacome@cern.ch>
2022-02-03 12:48:31 +0100

    added documentation to warp_pyecloud_sim.py

--- warp_pyecloud_sim.py
+++ warp_pyecloud_sim.py
@@ -25,30 +25,93 @@ from mpi4py import MPI
 
 
 class warp_pyecloud_sim(object):
-    __fieldsolver_inputs__ = {'nx': None, 'ny': None, 'nz': None, 'solver_type': 'ES', 'EM_method': 'Yee', 'cfl': 1.0,
-                              'dt': None, 'source_smoothing': True, 'l_pushf': False, 'l_getrho': False}
+    """
+    WarPyECLOUD simulation class.
+    - fieldsolver_inputs: input dictionary for the field solver
+    - beam_inputs: input dictionary for the proton beam
+    - ecloud_inputs: input dictionary for the electron cloud
+    - antenna_inputs: input dictionary for the laser antenna
+    - saving_inputs: input dictionary for the saver
+    - simulation_inputs: input dictionary for generic simulation parameters
+    """
+    __fieldsolver_inputs__ = {'nx': None,    # Number of cells in x-direction
+                              'ny': None,    # Number of cells in y-direction
+                              'nz': None,    # Number of cells in z-direction
+                              'solver_type': 'ES',    # Type of solver: ES or EM
+                              'EM_method': 'Yee',    # FDTD solver Method (Yee or CKC). (Used if solver_type == 'EM')
+                              'cfl': 1.0,    # CFL number of the FDTD solver. (Used if solver_type == 'EM')
+                              'dt': None,    # ES solver time step [s]. (Used if solver_type == 'ES')
+                              'source_smoothing': True,    # If True use source smoothing. (Used if solver_type == 'EM')
+                              'l_pushf': False,    # Use divE correction. (Used if solver_type == 'EM')
+                              'l_getrho': False    # If True gather also rho. (Used if solver_type == 'EM')
+                              }
 
-    __beam_inputs__ = {'n_bunches': None, 'b_spac': None, 'beam_gamma': None, 'sigmax': None, 'sigmay': None,
-                       'sigmat': None, 'bunch_intensity': None, 't_offs': None, 'bunch_macro_particles': 0,
-                       'custom_time_prof': None}
+    __beam_inputs__ = {'n_bunches': None,    # Number of bunches
+                       'b_spac': None,    # Bunch spacing [s]
+                       'beam_gamma': None,    # Gamma relativistic factor of the protons
+                       'sigmax': None,    # Beam std deviation in x-direction [m]
+                       'sigmay': None,    # Beam std deviation in y-direction [m]
+                       'sigmat': None,    # Beam std deviation in s-direction [s]
+                       'bunch_intensity': None,    # Number of protons per bunch
+                       't_offs': None,    # Offset of the max of the bunch distribution [s]
+                       'bunch_macro_particles': 0,    # Number of macroparticles in each bunch
+                       'custom_time_prof': None    # Custom time profile of the bunch (otherwise gaussian is assumed)
+                       }
 
-    __ecloud_inputs__ = {'init_num_elecs': None, 'init_num_elecs_mp': 0, 'pyecloud_nel_mp_ref': None,
-                         'pyecloud_fact_clean': None, 'pyecloud_fact_split': None, 'Emax': None, 'del_max': None,
-                         'R0': None, 'E_th': None, 'sigmafit': None, 'mufit': None,
-                         'secondary_angle_distribution': None, 'N_mp_max': None, 'N_mp_target': None,
-                         't_inject_elec': 0, 'init_ecloud_fields': False}
+    __ecloud_inputs__ = {'init_num_elecs': None,    # Initial number of electrons
+                         'init_num_elecs_mp': 0,    # Initial number of electron macroparticles
+                         'pyecloud_nel_mp_ref': None,    # Reference number of electrons per macroparticle
+                         'pyecloud_fact_clean': None,    # Minimum number of electrons per macroparticle
+                         'pyecloud_fact_split': None,    # Maximum number of electrons per macroparticle
+                         # The next 7 parameters are copied from PyECLOUD. Please refer to the PyECLOUD manual.
+                         'Emax': None,
+                         'del_max': None,
+                         'R0': None,
+                         'E_th': None,
+                         'sigmafit': None,
+                         'mufit': None,
+                         'secondary_angle_distribution': None,
+                         'N_mp_max': None,    # Maximum number of macroparticles in the cloud
+                         'N_mp_target': None,    # Target number of macroparticles in the cloud
+                         't_inject_elec': 0,    # Time at which the electrons are injected [s]
+                         'init_ecloud_fields': False    # If True initialize the electron cloud electric self-field
+                         }
 
-    __antenna_inputs__ = {'laser_source_z': None, 'laser_polangle': None, 'laser_emax': None, 'laser_xmin': None,
-                          'laser_xmax': None, 'laser_ymin': None, 'laser_ymax': None, 'laser_func': None}
+    __antenna_inputs__ = {'laser_source_z': None,    # z-coordinate of the laser antenna [m]
+                          'laser_polangle': None,    # Polarization angle of the laser antenna [rad]
+                          'laser_emax': None,    # Max of the E field on the antenna [V/m]
+                          'laser_xmin': None,    # Min x-coordinate of the laser antenna
+                          'laser_xmax': None,    # Max x-coordinate of the laser antenna
+                          'laser_ymin': None,    # Min x-coordinate of the laser antenna
+                          'laser_ymax': None,    # Max x-coordinate of the laser antenna
+                          'laser_func': None    # Laser function of the laser antenna
+                          }
 
-    __saving_inputs__ = {'flag_checkpointing': False, 'checkpoints': None, 'flag_output': True,
-                         'output_filename': 'output.h5', 'nbins': 100, 'radius': None, 'stride_imgs': 10,
-                         'stride_output': 1000, 'temps_filename': 'temp_mps_info.h5', 'custom_plot': None,
-                         'images_dir': 'images_dir', 'field_probes': [], 'field_probes_dump_stride': 1000,
-                         'probe_filename': 'probe.h5', 'flag_save_ek_impacts': False}
+    __saving_inputs__ = {'flag_checkpointing': False,    # If true save checkpoints
+                         'checkpoints': None,    # List of bunch passages when checkpoints are saved
+                         'flag_output': True,    # If True save outputs
+                         'output_filename': 'output.h5',    # Outputs file name
+                         'nbins': 100,    # Number of bins used for the histograms
+                         'stride_imgs': 10,    # Stride between two image save
+                         'stride_output': 1000,    # Stride between two outputs dumps
+                         'temps_filename': 'temp_mps_info.h5',    # Checkpoint file name
+                         'custom_plot': None,    # Custom plot function
+                         'images_dir': 'images_dir',    # Folder where to save images
+                         'field_probes': [],    # Locations of the field probes
+                         'field_probes_dump_stride': 1000,    # Stride between two field probe dumps
+                         'probe_filename': 'probe.h5',    # Probes file name
+                         'flag_save_ek_impacts': False    # if True save the kinetic energy deposited
+                                                          # on the conductors
+                         }
 
-    __simulation_inputs__ = {'enable_trap': True, 'flag_relativ_tracking': True, 'chamber': False, 'lattice_elem': None,
-                             'after_step_fun_list': [], 'tot_nsteps': None, 't_end': None}
+    __simulation_inputs__ = {'enable_trap': True, # If True enable text trap to reduce amount of outputs in the terminal
+                             'flag_relativ_tracking': True,    # If True enable relativistic tracking of particles
+                             'chamber': False,    # Chamber object
+                             'lattice_elem': None,    # Lattice element
+                             'after_step_fun_list': [],    # List of function to execute after a step
+                             'tot_nsteps': None,    # Total number of time steps. (Alternative to t_end)
+                             't_end': None    # End time of the simulation (Alternative to tot_nsteps)
+                             }
 
     def __init__(self, fieldsolver_inputs=None, beam_inputs=None, ecloud_inputs=None, antenna_inputs=None,
                  saving_inputs=None, simulation_inputs=None):
@@ -263,6 +326,9 @@ class warp_pyecloud_sim(object):
         self.print_solvers_info()
 
     def print_solvers_info(self):
+        """
+        Print information about the solvers
+        """
         # printing info about the sim
         print('dx = %e' % self.solver.solver.dx)
         print('dy = %e' % self.solver.solver.dy)
@@ -274,7 +340,13 @@ class warp_pyecloud_sim(object):
         print('EM solver: dt = %e' % dt_cfl)
         print('ES solver: dt = %e' % picmi.warp.top.dt)
 
-    def add_es_solver(self, deposition_species=[]):
+    def add_es_solver(self, deposition_species=None):
+        """
+        Add an electrostatic solver.
+        - deposition_species: list of species to be used by the ES solver
+        """
+        if deposition_species is None:
+            deposition_species = []
         grid_es = picmi.Cartesian3DGrid(number_of_cells=self.number_of_cells, lower_bound=self.lower_bound,
                                         upper_bound=self.upper_bound,  lower_boundary_conditions=self.dir_bc,
                                         upper_boundary_conditions=self.dir_bc)
@@ -287,7 +359,10 @@ class warp_pyecloud_sim(object):
         registersolver(self.es_solver.solver)
 
     def add_em_solver(self, deposition_species=None):
-
+        """
+        Add an electromagnetic solver.
+        - deposition_species: list of species to be used by the EM solver
+        """
         if deposition_species is None:
             deposition_species = []
         n_pass = [[1], [1], [1]]
@@ -310,6 +385,10 @@ class warp_pyecloud_sim(object):
         registersolver(self.em_solver.solver)
 
     def add_ms_solver(self, deposition_species=None):
+        """
+        Add a magnetostatic solver.
+        - deposition_species: list of species to be used by the MS solver
+        """
         if deposition_species is None:
             deposition_species = []
         grid_ms = picmi.Cartesian3DGrid(number_of_cells=self.number_of_cells, lower_bound=self.lower_bound,
@@ -323,6 +402,13 @@ class warp_pyecloud_sim(object):
         registersolver(self.ms_solver.solver)
 
     def distribute_species(self, primary_species=None, es_species=None, em_species=None, ms_species=None):
+        """
+        Redistribute species across the solvers
+        - primary_species: species used by self.solver
+        - es_species: species used by self.es_solver
+        - em_species: species used by self.em_solver
+        - ms_species: species used by self.ms_solver
+        """
         if primary_species is not None:
             self.solver.solver.deposition_species = primary_species
         if es_species is not None:
@@ -336,6 +422,10 @@ class warp_pyecloud_sim(object):
         self.saver.update_field_probes(self.field_probes)
 
     def step(self, u_steps=1):
+        """
+        Perform u_steps with electron cloud specific checks
+        - u_steps: number of steps
+        """
         for u_step in range(u_steps):
             # if a passage is starting...
             if self.n_step % self.ntsteps_p_bunch == 0:
@@ -397,12 +487,17 @@ class warp_pyecloud_sim(object):
                 print('Run terminated in %ds' % totalt)
 
     def all_steps(self):
+        """
+        Perform all steps with electron cloud specific checks
+        """
         for i in range(self.n_step, self.tot_nsteps):
             self.step()
             self.n_step += 1
-            # print(self.beam.wspecies.getn())
 
     def all_steps_no_ecloud(self):
+        """
+        Perform all steps without electron cloud specific checks
+        """
         if picmi.warp.me == 0:
             for _ in tqdm(range(self.n_step, self.tot_nsteps)):
                 sys.stdout = self.text_trap
@@ -437,6 +532,9 @@ class warp_pyecloud_sim(object):
                 self.n_step += 1
 
     def uniform_density(self):
+        """
+        Uniform density function
+        """
         init_num_elecs_mp = self.init_num_elecs_mp
 
         x0 = np.empty(init_num_elecs_mp, dtype=float)
@@ -482,6 +580,9 @@ class warp_pyecloud_sim(object):
         return x0, y0, z0, vx0, vy0, vz0, gi0, w0
 
     def init_uniform_density(self):
+        """
+        initialize uniform density of electrons
+        """
         pwt = picmi.warp.top
         if np.isclose(pwt.time, self.t_inject_elec, rtol=0, atol=pwt.dt) and (pwt.time - self.t_inject_elec) > 0:
             x0, y0, z0, vx0, vy0, vz0, gi0, w0 = self.uniform_density()
@@ -497,6 +598,9 @@ class warp_pyecloud_sim(object):
             print('injected %d MPs' % self.ecloud.wspecies.getn())
 
     def load_elec_density(self):
+        """
+        Load density of electrons from checkpoint file
+        """
         print('#############################################################')
         print('Temp distribution found. Reloading it as initial distribution')
         print('#############################################################')
@@ -517,6 +621,9 @@ class warp_pyecloud_sim(object):
                                               weight=w0)
 
     def gaussian_time_prof(self):
+        """
+        Time profile of a Gaussian bunch
+        """
         t = picmi.warp.top.time
         val = 0
         for i in range(0, self.n_bunches):
@@ -527,6 +634,9 @@ class warp_pyecloud_sim(object):
         return val
 
     def bunched_beam(self):
+        """
+        Inject a bunched beam
+        """
         n_p = int(np.round(self.time_prof()))
         if n_p > 0:
             x = random.normal(self.bunch_centroid_position[0],
@@ -559,6 +669,10 @@ class warp_pyecloud_sim(object):
                                 '%s' is out of place""" % list(kw)[0])
 
     def dump(self, filename):
+        """
+        Dump the simulaton to a pkl file.
+        - filename: name of the pkl file
+        """
         if hasattr(self, 'laser_func'):
             del self.solver.em3dfft_args['laser_func']
             self.laser_func = None
